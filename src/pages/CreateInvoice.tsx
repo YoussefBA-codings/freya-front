2250973ecd588a6cda8cf005ebd93934a58c1c74
Youssef <benamoyoussef@gmail.com>
2024-10-30 08:02:49 +0100

    add create-sync

--- src/pages/CreateInvoice.tsx
+++ src/pages/CreateInvoice.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 import {
   Snackbar,
   SnackbarContent,
@@ -6,326 +7,100 @@ import {
   Button,
   Box,
   Typography,
-  Grid,
   CircularProgress,
 } from "@mui/material";
-export interface InvoiceData {
-  shipping_address: {
-    first_name: string;
-    address1: string;
-    city: string;
-    zip: string;
-    country: string;
-  };
-  name: string;
-  processed_at: string;
-  line_items: {
-    sku?: string;
-    name: string;
-    quantity: number;
-    price: number;
-  }[];
-  shipping_lines: {
-    price: number;
-  }[];
-  current_total_discounts: number;
-  invoice_forced_name?: string; // Ajout du champ optionnel
-}
 
-const CreateInvoice: React.FC = () => {
-  const [formData, setFormData] = useState({
-    first_name: "",
-    address1: "",
-    city: "",
-    zip: "",
-    country: "",
-    name: "",
-    processed_at: new Date().toISOString(),
-    line_items: [{ sku: "", name: "", quantity: 1, price: 0 }],
-    total_shipping_price: 0,
-    current_total_discounts: 0,
-    invoice_forced_name: "", // Champ optionnel pour le nom de la facture
-  });
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
+const SyncInvoice: React.FC = () => {
+  const [invoiceShopifyId, setInvoiceShopifyId] = useState("");
+  const [invoiceForcedName, setInvoiceForcedName] = useState<string>("");
   const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [notifyMessage, setNotifyMessage] = useState("");
   const [notifyStatus, setNotifyStatus] = useState<"success" | "error">("success");
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, index?: number) => {
-    const { name, value } = e.target;
-
-    if (index !== undefined) {
-      const updatedItems = formData.line_items.map((item, idx) =>
-        idx === index ? { ...item, [name]: value } : item
-      );
-      setFormData({ ...formData, line_items: updatedItems });
-    } else {
-      setFormData({ ...formData, [name]: value });
-    }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInvoiceShopifyId(e.target.value);
   };
 
-  const addItem = () => {
-    setFormData({
-      ...formData,
-      line_items: [...formData.line_items, { sku: "", name: "", quantity: 1, price: 0 }],
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSync = async () => {
     setLoading(true);
-
-    const invoiceData: InvoiceData = {
-      shipping_address: {
-        first_name: formData.first_name,
-        address1: formData.address1,
-        city: formData.city,
-        zip: formData.zip,
-        country: formData.country,
-      },
-      name: formData.name,
-      processed_at: formData.processed_at,
-      line_items: formData.line_items.map((item) => ({
-        sku: item.sku,
-        name: item.name,
-        quantity: Number(item.quantity),
-        price: Number(item.price),
-      })),
-      shipping_lines: [{
-        price:  Number(formData.total_shipping_price),
-      }],
-      current_total_discounts: Number(formData.current_total_discounts),
-      invoice_forced_name: formData.invoice_forced_name,
-    };
+    setProgress(0);
+    setNotifyMessage("");
+    setNotifyStatus("success");
 
     try {
-      const response = await fetch(import.meta.env.VITE_API_URL + "invoices/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(invoiceData),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to create invoice");
+      for (let i = 0; i <= 100; i += 10) {
+        setProgress(i);
+        await new Promise((resolve) => setTimeout(resolve, 200));
       }
 
-      setNotifyMessage("Invoice created successfully!");
-      setNotifyStatus("success");
-      setSnackbarOpen(true);
+      await axios.post(
+        `${import.meta.env.VITE_API_URL}invoices/create-sync`,
+        {
+          orderShopifyId: invoiceShopifyId,
+          invoiceForcedName: invoiceForcedName || undefined,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-      // Reset form after successful submission
-      setFormData({
-        first_name: "",
-        address1: "",
-        city: "",
-        zip: "",
-        country: "",
-        name: "",
-        processed_at: new Date().toISOString(),
-        line_items: [{ sku: "", name: "", quantity: 1, price: 0 }],
-        total_shipping_price: 0,
-        current_total_discounts: 0,
-        invoice_forced_name: "", // Réinitialiser le champ optionnel
-      });
+      setNotifyMessage("Invoice synced successfully!");
+      setNotifyStatus("success");
     } catch (error) {
-      setNotifyMessage("Error creating invoice, please check logs!");
+      console.error("Failed to create invoice:", error);
+      setNotifyMessage("Failed to create invoice.");
       setNotifyStatus("error");
-      console.error("Error creating invoice:", error);
-      setSnackbarOpen(true);
     } finally {
       setLoading(false);
+      setSnackbarOpen(true);
     }
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ padding: 3, borderRadius: 2, boxShadow: 3 }}>
-        <Typography variant="h2" gutterBottom>
-        Create Invoice 
+    <Box sx={{ padding: 3, borderRadius: 2, boxShadow: 3, textAlign: "center" }}>
+      <Typography variant="h4" gutterBottom>
+        Create Invoice
       </Typography>
-      <Typography variant="h5" gutterBottom>Shipping Information</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="First Name"
-            name="first_name"
-            value={formData.first_name}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Address"
-            name="address1"
-            value={formData.address1}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="City"
-            name="city"
-            value={formData.city}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Zip Code"
-            name="zip"
-            value={formData.zip}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Country"
-            name="country"
-            value={formData.country}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-      </Grid>
-
-      <Typography variant="h5" gutterBottom sx={{ marginTop: 3 }}>Invoice Details</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Order Name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="datetime-local"
-            name="processed_at"
-            value={formData.processed_at}
-            onChange={handleInputChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Forced Invoice Name (Optional)"
-            name="invoice_forced_name"
-            value={formData.invoice_forced_name}
-            onChange={handleInputChange}
-          />
-        </Grid>
-      </Grid>
-
-      <Typography variant="h5" gutterBottom sx={{ marginTop: 3 }}>Line Items</Typography>
-      {formData.line_items.map((item, index) => (
-        <Grid container spacing={2} key={index}>
-          <Grid item xs={12} sm={4}>
-            <TextField
-              fullWidth
-              variant="outlined"
-              label="SKU"
-              name="sku"
-              value={item.sku}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, index)}
-            />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <TextField
-              fullWidth
-              variant="outlined"
-              label="Product Name"
-              name="name"
-              value={item.name}
-              onChange={(e:React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, index)}
-              required
-            />
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <TextField
-              fullWidth
-              variant="outlined"
-              type="number"
-              label="Quantity"
-              name="quantity"
-              value={item.quantity}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, index)}
-              required
-            />
-          </Grid>
-          <Grid item xs={12} sm={2}>
-            <TextField
-              fullWidth
-              variant="outlined"
-              type="number"
-              name="price"
-              value={item.price}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, index)}
-              required
-            />
-          </Grid>
-        </Grid>
-      ))}
-      <Button variant="outlined" onClick={addItem} sx={{ marginTop: 2 }}>
-        Add Item
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Invoice Shopify ID"
+        name="invoiceShopifyId"
+        value={invoiceShopifyId}
+        onChange={handleInputChange}
+        required
+        sx={{ marginBottom: 3 }}
+      />
+      <TextField
+        fullWidth
+        variant="outlined"
+        label="Invoice Forced Name (Optional)"
+        name="invoiceForcedName"
+        value={invoiceForcedName}
+        onChange={(e) => setInvoiceForcedName(e.target.value)}
+        sx={{ marginBottom: 3 }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSync}
+        disabled={loading}
+        sx={{ position: "relative" }}
+      >
+        {loading ? (
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <CircularProgress size={24} sx={{ marginRight: 2 }} />
+            {`${progress}% Retrieving from Shopify`}
+          </Box>
+        ) : (
+          "Create"
+        )}
       </Button>
 
-      <Typography variant="h5" gutterBottom sx={{ marginTop: 3 }}>Additional Costs</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="number"
-            label="Total Shipping Price"
-            name="total_shipping_price"
-            value={formData.total_shipping_price}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            type="number"
-            label="Total Discounts"
-            name="current_total_discounts"
-            value={formData.current_total_discounts}
-            onChange={handleInputChange}
-            required
-          />
-        </Grid>
-      </Grid>
-
-      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}>
-        <Button variant="contained" color="primary" type="submit" disabled={loading}>
-          {loading ? <CircularProgress size={24} /> : "Create Invoice"}
-        </Button>
-      </Box>
-
-    
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={3000}
@@ -333,11 +108,11 @@ const CreateInvoice: React.FC = () => {
       >
         <SnackbarContent
           message={notifyMessage}
-          sx={{ backgroundColor: notifyStatus === 'error' ? 'red' : 'green'}}
+          sx={{ backgroundColor: notifyStatus === "error" ? "red" : "green" }}
         />
       </Snackbar>
     </Box>
   );
 };
 
-export default CreateInvoice;
+export default SyncInvoice;
