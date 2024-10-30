06fb007efc035dc2dac98ce90a2529488b765792
Youssef <benamoyoussef@gmail.com>
2024-10-30 13:35:06 +0100

    add export

--- src/pages/OrdersToProcess.tsx
+++ src/pages/OrdersToProcess.tsx
@@ -12,8 +12,11 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Checkbox,
 } from "@mui/material";
 import PrepareInvoiceDetailsDrawer from "../epic/invoices/PrepareInvoiceDetailsDrawer";
+import { saveAs } from "file-saver";
+import Papa from "papaparse";
 
 interface Invoice {
   id: string;
@@ -27,7 +30,7 @@ interface Invoice {
   };
   billing_address: {
     address1: string;
-    city: string
+    city: string;
   };
   totalDiscount: number;
   shippingAmount: number;
@@ -36,6 +39,7 @@ interface Invoice {
     name: string;
     current_quantity: number;
     price: number;
+    vendor: string
   }>;
 }
 
@@ -43,11 +47,12 @@ const OrdersToProcess: React.FC = () => {
   const [invoices, setInvoices] = useState<Invoice[]>([]);
   const [open, setOpen] = useState(false);
   const [selectedInvoice, setSelectedInvoice] = useState<Invoice | null>(null);
+  const [selectedOrders, setSelectedOrders] = useState<Set<string>>(new Set());
 
   useEffect(() => {
     const fetchInvoices = async () => {
       try {
-        const response = await axios.get( `${import.meta.env.VITE_API_URL}invoices/order-to-treat`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}invoices/order-to-treat`);
         setInvoices(response.data);
       } catch (error) {
         console.error("Error fetching invoices:", error);
@@ -67,6 +72,71 @@ const OrdersToProcess: React.FC = () => {
     setSelectedInvoice(null);
   };
 
+  const handleSelectOrder = (id: string) => {
+    const newSelectedOrders = new Set(selectedOrders);
+    if (newSelectedOrders.has(id)) {
+      newSelectedOrders.delete(id);
+    } else {
+      newSelectedOrders.add(id);
+    }
+    setSelectedOrders(newSelectedOrders);
+  };
+
+  const handleExport = () => {
+    const selectedInvoices = invoices.filter((invoice) => selectedOrders.has(invoice.id));
+  
+    const csvData = selectedInvoices.map((invoice) => {
+      const customerDetails = [
+        invoice.customer.first_name ? `Prénom: ${invoice.customer.first_name}` : "",
+        invoice.customer.last_name ? `Nom: ${invoice.customer.last_name}` : "",
+        invoice.customer.phone ? `Téléphone: ${invoice.customer.phone}` : "",
+        invoice.customer.email ? `Email: ${invoice.customer.email}` : ""
+      ].filter(Boolean).join('\n');
+
+      const gbDistributionItems = invoice.line_items.filter(
+        (item) =>
+          item.sku &&
+          item.sku !== undefined &&
+          item.sku !== "" &&
+          !item.sku.startsWith("B_")
+      );
+  
+      const blackItems = invoice.line_items.filter(
+        (item) => !gbDistributionItems.includes(item)
+      );
+  
+      const gbTotalPrice = gbDistributionItems.reduce((total, item) => {
+        return total + item.price * item.current_quantity;
+      }, 0);
+  
+      const blackTotalPrice = blackItems.reduce((total, item) => {
+        return total + item.price * item.current_quantity;
+      }, 0);
+  
+      const combinedTotal = gbTotalPrice + blackTotalPrice;
+  
+      const gbProductsDetails = gbDistributionItems.map((item) => ({
+        "Produit": `${item.vendor} - ${item.name}`,
+        "Quantité": item.current_quantity,
+      }));
+  
+      const blackProductsDetails = blackItems.map((item) => ({
+        "Produit": `${item.vendor} - ${item.name}`,
+        "Quantité": item.current_quantity,
+      }));
+  
+      return {
+        "Client": customerDetails,
+        "Produits GB Distribution": gbProductsDetails.map(p => `${p.Produit} (${p.Quantité})`).join(', '),
+        "Produits Black": blackProductsDetails.map(p => `${p.Produit} (${p.Quantité})`).join(', '),
+        "Total TTC": combinedTotal.toFixed(2), 
+      };
+    });
+  
+    const csv = Papa.unparse(csvData);
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    saveAs(blob, "fulfillment.csv");
+  };    
   return (
     <Box padding={3}>
       <Typography variant="h4" gutterBottom>
@@ -75,10 +145,30 @@ const OrdersToProcess: React.FC = () => {
 
       <Divider />
 
+      <Button
+        variant="contained"
+        onClick={handleExport}
+        disabled={selectedOrders.size === 0}
+        sx={{ margin: "16px 0" }}
+      >
+        Exporter Sélection
+      </Button>
+
       <TableContainer component={Paper} sx={{ marginTop: 2 }}>
         <Table>
           <TableHead>
             <TableRow>
+              <TableCell padding="checkbox">
+                <Checkbox
+                  checked={selectedOrders.size === invoices.length}
+                  onChange={() => {
+                    const newSelectedOrders: Set<string> = selectedOrders.size === invoices.length 
+                      ? new Set() 
+                      : new Set(invoices.map((invoice) => invoice.id));
+                    setSelectedOrders(newSelectedOrders);
+                  }}
+                />
+              </TableCell>
               <TableCell>Shopify Order ID</TableCell>
               <TableCell>Numéro de commande</TableCell>
               <TableCell>Date de traitement</TableCell>
@@ -89,15 +179,18 @@ const OrdersToProcess: React.FC = () => {
           <TableBody>
             {invoices.map((invoice) => (
               <TableRow key={invoice.id}>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedOrders.has(invoice.id)}
+                    onChange={() => handleSelectOrder(invoice.id)}
+                  />
+                </TableCell>
                 <TableCell>{invoice.id}</TableCell>
                 <TableCell>{invoice.name}</TableCell>
                 <TableCell>{invoice.processed_at.split("T")[0]}</TableCell>
                 <TableCell>{`${invoice.customer.first_name} ${invoice.customer.last_name}`}</TableCell>
                 <TableCell>
-                  <Button
-                    variant="contained"
-                    onClick={() => handleOpen(invoice)}
-                  >
+                  <Button variant="contained" onClick={() => handleOpen(invoice)}>
                     Voir Détails
                   </Button>
                 </TableCell>
